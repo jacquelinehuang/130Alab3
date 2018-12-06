1b8fa2d475997c51e48a495bd1c7b3b0d0ffa332
Jacqueline Huang <jacquelinehuang@umail.ucsb.edu>
2018-12-06 05:54:21 -0800

    i give up

--- BtreeTest.cpp
+++ BtreeTest.cpp
@@ -8,8 +8,6 @@ using std::endl;
 //tester class to make sure btree works properly. not quite sure how to write this test class yet either
 int main ()
 {
-	int tracker;
-
 	std::ifstream ifs;
 	
 	std::string userInfo;
@@ -18,7 +16,7 @@ int main ()
 
 	std::string name, genre1, genre2;
 	int userPerm;
-	std::vector<Entry*> entrymaker;
+	std::vector<Entry*> entrymaker, entrymaker2;
 	
 	
 	ifs.open("textFile.txt");
@@ -52,11 +50,8 @@ int main ()
 		
 	}
 
-	//node making test
-	//Btree *temp = new Btree (new Entry (new User (123, "aa","aa","aa"),1) );
-	//cout<<"make test tree with  perm: 123"<<endl;
 
-
+	ifs.close();
 	
 	Btree *aBTree=new Btree(entrymaker[0]);	
 	cout<<"make tree with  perm: "<<entrymaker[0]->getkey()<<endl;
@@ -68,7 +63,80 @@ int main ()
 		//cout<<"TESTCLASS MESSAGE inserted perm: "<<entrymaker[i]->getkey()<<endl;	
 
 	}
-	//please work
+
+	cout<<"insert new perms"<<endl;
+	ifs.open("weirdtextfile.txt");
+	if (ifs.fail()){
+		std::cout<< "Couldn't find text file" << std::endl;
+	}
+	
+	//read whileloops
+	while (!ifs.eof()) { 
+		std::getline(ifs, userInfo);
+
+		pos = userInfo.find(semicolon);
+		userPerm = std::stoi(userInfo.substr(0, pos));
+		userInfo.erase(0, pos + semicolon.length());
+			
+		pos = userInfo.find(semicolon);
+		name = userInfo.substr(0, pos);
+		userInfo.erase(0, pos + semicolon.length());
+		
+		pos = userInfo.find(semicolon);
+		genre1 = userInfo.substr(0, pos);
+		userInfo.erase(0, pos + semicolon.length());
+		
+		pos = userInfo.find(semicolon);
+		genre2 = userInfo.substr(0, pos);
+		userInfo.erase(0, pos + semicolon.length());
+
+		//make entries from data read with default position 1. 
+		//cout<<"perm name are:"<<userPerm<<" "<<name<<endl;
+		entrymaker2.push_back(new Entry (new User(userPerm, name, genre1, genre2), 1) );
+		
+	}
+
+	for (unsigned int i=0; i < entrymaker2.size(); i++)
+	 {
+		//inserting in a graph and a tree
+		aBTree->insert(entrymaker2[i]);
+		//cout<<"TESTCLASS MESSAGE inserted perm: "<<entrymaker[i]->getkey()<<endl;	
+
+	}
+
+	cout<<"out of loop2"<<endl;
+
+//rando order  test
+
+	aBTree->insert(new Entry (new User (999, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (607, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (110, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (1482, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (108, "aa","aa","aa"),1) );	
+	aBTree->insert(new Entry (new User (994, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (244, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (0, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (153, "aa","aa","aa"),1) );
+	//problemarises here 
+	aBTree->insert(new Entry (new User (125, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (199, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (110, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (1092, "aa","aa","aa"),1) );
+	aBTree->insert(new Entry (new User (385, "aa","aa","aa"),1) );
+
+
+	cout<<aBTree->search(1231)->getkey()<<aBTree->search(1231)->getuser()->getName()<<endl;
+	cout<<aBTree->search(142)->getkey()<<aBTree->search(142)->getuser()->getName()<<endl;
+	cout<<aBTree->search(993)->getkey()<<aBTree->search(993)->getuser()->getName()<<endl;
+	cout<<aBTree->search(108)->getkey()<<aBTree->search(108)->getuser()->getName()<<endl;
+	cout<<aBTree->search(109)->getkey()<<aBTree->search(109)->getuser()->getName()<<endl;
+	cout<<aBTree->search(116)->getkey()<<aBTree->search(116)->getuser()->getName()<<endl;
+	cout<<aBTree->search(1117)->getkey()<<aBTree->search(1117)->getuser()->getName()<<endl;
+	cout<<aBTree->search(900)->getkey()<<aBTree->search(900)->getuser()->getName()<<endl;
+	cout<<aBTree->search(607)->getkey()<<aBTree->search(607)->getuser()->getName()<<endl;
+	cout<<aBTree->search(153)->getkey()<<aBTree->search(153)->getuser()->getName()<<endl;
+
+		//please work
 	cout<< "\nsearching for, 1 returns: " <<aBTree->search(1)->getkey()<<endl;
 	cout<< "searching for 8, returns: " <<aBTree->search(8)->getkey()<<endl;
 	cout<< "searching for, 5 returns: "<< aBTree->search(5)->getkey()<<endl;
@@ -80,36 +148,20 @@ int main ()
 	cout<< "searching for, 57 returns: " <<aBTree->search(57)->getkey()<<endl;
 	cout<< "searching for, 107 returns: " <<aBTree->search(107)->getkey()<<endl;
 	cout<< "searching for, 89 returns: " <<aBTree->search(89)->getkey()<<endl;
-
-		//insert to see if it picksup somethiing is wrong 
-	cout<<"make duplicate perm. what does it say?"<<endl;
-	aBTree->insert(new Entry (new User (1, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (3, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (24, "aa","aa","aa"),1) );
-
-	cout<<"insert new perms"<<endl;
-	aBTree->insert(new Entry (new User (108, "aa","aa","aa"),1) );	
-	aBTree->insert(new Entry (new User (154, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (244, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (0, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (173, "aa","aa","aa"),1) );
-		tracker=125;
-	aBTree->insert(new Entry (new User (125, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (109, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (110, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (111, "aa","aa","aa"),1) );
-	aBTree->insert(new Entry (new User (305, "aa","aa","aa"),1) );
-
-	cout<< "searching for, 154 returns: " <<aBTree->search(154)->getkey()<<endl;
-	cout<< "searching for, 244 returns: " <<aBTree->search(244)->getkey()<<endl;
-	cout<< "searching for, 0 returns: " <<aBTree->search(0)->getkey()<<endl;
-	cout<< "searching for, 173 returns: " <<aBTree->search(173)->getkey()<<endl;
+	cout<< "searching for, 154 returns: " <<aBTree->search(999)->getkey()<<endl;
+	cout<< "searching for, 244 returns: " <<aBTree->search(607)->getkey()<<endl;
+	cout<< "searching for, 0 returns: " <<aBTree->search(110)->getkey()<<endl;
+	cout<< "searching for, 173 returns: " <<aBTree->search(1482)->getkey()<<endl;
 	cout<< "searching for, 108 returns: " <<aBTree->search(108)->getkey()<<endl;
-	cout<< "searching for, 125 returns: " <<aBTree->search(125)->getkey()<<endl;
-	cout<< "searching for, 109 returns: " <<aBTree->search(109)->getkey()<<endl;
+	cout<< "searching for, 125 returns: " <<aBTree->search(994)->getkey()<<endl;
+	cout<< "searching for, 109 returns: " <<aBTree->search(244)->getkey()<<endl;
+	cout<< "searching for, 110 returns: " <<aBTree->search(0)->getkey()<<endl;
+	cout<< "searching for, 111 returns: " <<aBTree->search(153)->getkey()<<endl;	
+	cout<< "searching for, 305 returns: " <<aBTree->search(125)->getkey()<<endl;
+	cout<< "searching for, 125 returns: " <<aBTree->search(199)->getkey()<<endl;
 	cout<< "searching for, 110 returns: " <<aBTree->search(110)->getkey()<<endl;
-	cout<< "searching for, 111 returns: " <<aBTree->search(111)->getkey()<<endl;	
-	cout<< "searching for, 305 returns: " <<aBTree->search(305)->getkey()<<endl;
-	
+	cout<< "searching for, 111 returns: " <<aBTree->search(1092)->getkey()<<endl;	
+	cout<< "searching for, 305 returns: " <<aBTree->search(385)->getkey()<<endl;
+
 	return 0;
 }
